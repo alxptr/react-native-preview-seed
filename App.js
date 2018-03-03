642db9e190b387747e0f8be806b78facadb50c04
apoterenko <a.poterenko@gmail.com>
2018-03-04 00:50:51 +0300

    notifications

--- App.js
+++ App.js
@@ -1,15 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from 'react-native';
+import {
+  NotificationsAndroid,
+  PendingNotifications,
+} from 'react-native-notifications';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.onSendNotification = this.onSendNotification.bind(this);
+    this.onCancelNotification = this.onCancelNotification.bind(this);
+    this.onPressClick = this.onPressClick.bind(this);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+          onPress={this.onPressClick}
+          title="Send a local notification"
+          color="#78ff9b"
+        />
       </View>
     );
+  }
+
+  onPressClick() {
+    this.onSendNotification('This is a first notification!');
+  }
+
+  onSendNotification(text) {
+    this.notificationId = NotificationsAndroid.localNotification({ title: 'Test notification', body: text });
+  }
+
+  onCancelNotification() {
+    if (this.notificationId) {
+      NotificationsAndroid.cancelLocalNotification(this.notificationId);
+      this.notificationId = null;
+    }
   }
 }
 
@@ -21,3 +58,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+AppRegistry.registerComponent('WixRNNotifications', () => App);
